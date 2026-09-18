7979437527cd3a80fe44f742baf8df6a253c3d54
Maria Jose Bolaños <mariajbguerra93@gmail.com>
2026-09-17 19:39:53 -0500

    Add domain entities, AppDbContext and initial migration (Module 2)

---
+++ GolBet.Entities/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using GolBet.Entities.Common;
+
+namespace GolBet.Entities;
+
+public class Team : AuditableEntity
+{
+    [Required, MaxLength(80)]
+    public string Name { get; set; } = null!;
+
+    [Required, MaxLength(60)]
+    public string City { get; set; } = null!;
+
+    [MaxLength(300)]
+    public string? CrestUrl { get; set; }
+}
+
